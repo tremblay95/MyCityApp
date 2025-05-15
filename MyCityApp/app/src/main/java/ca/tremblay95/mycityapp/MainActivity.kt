4d59d18963eb144a787fcd95402183e0502e3dea
marct <marc.tremblay1995@gmail.com>
2025-05-14 22:44:40 -0400

    material theme setup

--- MyCityApp/app/src/main/java/ca/tremblay95/mycityapp/MainActivity.kt
+++ MyCityApp/app/src/main/java/ca/tremblay95/mycityapp/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ca.tremblay95.mycityapp.ui.theme.MyCityAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +37,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(8.dp)
+    ) {
+        Text(
+            text = "Hello $name!",
+            style = MaterialTheme.typography.displayLarge
+        )
+        Text(
+            text = "Welcome to ${stringResource(R.string.app_name)}",
+            style = MaterialTheme.typography.titleLarge
+        )
+    }
 }
 
 @Preview(showBackground = true)
--- MyCityApp/app/src/main/java/ca/tremblay95/mycityapp/ui/theme/Type.kt
+++ MyCityApp/app/src/main/java/ca/tremblay95/mycityapp/ui/theme/Type.kt
@@ -2,33 +2,58 @@ package ca.tremblay95.mycityapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import ca.tremblay95.mycityapp.R
+
+val Allan = FontFamily(
+    Font(R.font.allan_regular),
+    Font(R.font.allan_bold, FontWeight.Bold)
+)
+val Alef = FontFamily(
+    Font(R.font.alef_regular),
+    Font(R.font.alef_bold, FontWeight.Bold)
+)
+
+val baseline = Typography()
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    /*displayLarge = TextStyle(
+        fontFamily = Allan,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Allan,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
-)
+        fontFamily = Alef,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Alef,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+    )*/
+    displayLarge = baseline.displayLarge.copy(fontFamily = Allan),
+    displayMedium = baseline.displayMedium.copy(fontFamily = Allan),
+    displaySmall = baseline.displaySmall.copy(fontFamily = Allan),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = Allan),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = Allan),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = Allan),
+    titleLarge = baseline.titleLarge.copy(fontFamily = Allan),
+    titleMedium = baseline.titleMedium.copy(fontFamily = Allan),
+    titleSmall = baseline.titleSmall.copy(fontFamily = Allan),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = Alef),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = Alef),
+    bodySmall = baseline.bodySmall.copy(fontFamily = Alef),
+    labelLarge = baseline.labelLarge.copy(fontFamily = Alef),
+    labelMedium = baseline.labelMedium.copy(fontFamily = Alef),
+    labelSmall = baseline.labelSmall.copy(fontFamily = Alef),
+)   
