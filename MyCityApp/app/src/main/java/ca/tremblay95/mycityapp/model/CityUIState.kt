7847bfebfe486250605ddef26ec98d691230d6a0
marct <marc.tremblay1995@gmail.com>
2025-05-20 14:44:16 -0400

    viewmodel and state

---
+++ MyCityApp/app/src/main/java/ca/tremblay95/mycityapp/model/CityUIState.kt
@@ -0,0 +1,9 @@
+package ca.tremblay95.mycityapp.model
+
+import ca.tremblay95.mycityapp.data.Category
+import ca.tremblay95.mycityapp.data.LocalCityDataProvider
+
+data class CityUIState(
+    val currentCategory: Category = LocalCityDataProvider.defaultCategory,
+    val currentPlace: Place = LocalCityDataProvider.defaultPlace
+)
--- MyCityApp/app/src/main/java/ca/tremblay95/mycityapp/ui/MyCityViewModel.kt
+++ MyCityApp/app/src/main/java/ca/tremblay95/mycityapp/ui/MyCityViewModel.kt
@@ -1,4 +1,31 @@
 package ca.tremblay95.mycityapp.ui
 
-class MyCityViewModel {
+import androidx.lifecycle.ViewModel
+import ca.tremblay95.mycityapp.data.Category
+import ca.tremblay95.mycityapp.data.LocalCityDataProvider
+import ca.tremblay95.mycityapp.model.CityUIState
+import ca.tremblay95.mycityapp.model.Place
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
+class MyCityViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(CityUIState())
+    val uiState: StateFlow<CityUIState> = _uiState
+
+    fun updateCurrentPlace(selectedPlace: Place) {
+        _uiState.update {
+            it.copy(currentPlace = selectedPlace)
+        }
+    }
+
+    fun updateCurrentCategory(selectedCategory: Category) {
+        _uiState.update {
+            it.copy(
+                currentCategory = selectedCategory,
+                currentPlace = LocalCityDataProvider.getPlacesData(selectedCategory).first(),
+            )
+        }
+    }
 }
