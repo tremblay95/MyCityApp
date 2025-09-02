da057a0a99cf4c34db566506f3937838d9fed403
marct <marc.tremblay1995@gmail.com>
2025-09-02 17:04:02 -0400

    wip viewmodel tests

---
+++ MyCityApp/app/src/test/java/ca/tremblay95/mycityapp/MyCityViewModelTests.kt
@@ -0,0 +1,55 @@
+package ca.tremblay95.mycityapp
+
+import org.junit.Test
+
+class MyCityViewModelTests {
+    @Test
+    fun updateCurrentPlace_stateUpdatedCorrectly() {
+
+    }
+
+    @Test
+    fun updateCurrentCategory_stateUpdatedCorrectly() {
+
+    }
+
+    @Test
+    fun getCategoriesList_categoriesListReturned() {
+
+    }
+
+    @Test
+    fun getPlacesList_defaultCategory_correctPlacesListReturned() {
+
+    }
+
+    @Test
+    fun getPlacesList_categoryChanged_correctPlacesListReturned() {
+
+    }
+
+    @Test
+    fun getCurrentPlace_defaultPlace_correctPlaceReturned() {
+
+    }
+
+    @Test
+    fun getCurrentPlace_placeChanged_correctPlaceReturned() {
+
+    }
+
+    @Test
+    fun getCurrentPlace_categoryChanged_placeInCorrectCategory() {
+
+    }
+
+    @Test
+    fun getNavBarInfo_categoriesListScreen_correctLabelAndImageResources() {
+
+    }
+
+    @Test
+    fun getNavBarInfo_placesListScreen_correctLabelAndIconResources() {
+
+    }
+}
